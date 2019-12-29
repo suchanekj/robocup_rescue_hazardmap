3a062812394397c8f83354345bbd3f0e20a0750a
jon-chuang <chuang.jon@gmail.com>
2019-12-29 23:07:18 +0000

    3 workers

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ def train_cycle(model, lrs, epochs, current_epoch, lines, num_train, num_val, in
                             steps_per_epoch=max(1, num_train // batch_size // 10),
                             epochs=1,
                             initial_epoch=0,
-                            workers=4,
+                            workers=3,
                             max_queue_size=10)
 
         opt = Adam(lr=lr*hvd.size())
@@ -83,7 +83,7 @@ def train_cycle(model, lrs, epochs, current_epoch, lines, num_train, num_val, in
                             epochs=current_epoch + epoch - skip,
                             initial_epoch=current_epoch,
                             callbacks=callbacks,
-                            workers=4,
+                            workers=3,
                             max_queue_size=10)
         current_epoch += epoch
     return model, current_epoch
